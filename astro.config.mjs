6bfb7e3098ae4096fcfdfef7843d31e87ee20ff3
dpwl35 <dpwl9435@gmail.com>
2025-08-16 17:50:41 +1000

    chore: 초기 세팅 (Astro + Starlight 환경 구성)

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "YeJi's Notes",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/dpwl35/dpwl35.github.io",
+        },
+      ],
+    }),
+  ],
+});
---
+++ src/content/docs/Three/three.md
@@ -0,0 +1,6 @@
+---
+title: three.js 시작하기
+description: TDD 자료 모음
+---
+
+## 학습
